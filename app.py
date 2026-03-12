da1367bc385adafc89c72244b39cd862e8734f06
SaribJunaid <saribjunaid5@gmail.com>
2026-03-12 23:12:24 +0500

    add sys path in app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from payroll_app.ui import run_app
+import sys
+import os
+sys.path.insert(0, os.path.dirname(__file__))
 
+from payroll_app.ui import run_app
 
 if __name__ == "__main__":
     run_app()
-
